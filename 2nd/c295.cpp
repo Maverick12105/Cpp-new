5a595f8e82e95f04ed1422b0738b65ba93687f63
Maverick <mike940613@gmail.com>
2025-03-15 17:59:58 +0800

    Update

--- 2nd/c295.cpp
+++ 2nd/c295.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int N, M, xi[20][20], S = 0, N_max[20];
+    int N, M, xi[20][20], S = 0, N_max[20], divisible = 0, first = 1;
 
     cin >> N >> M;
     for (int i = 0; i < N; ++i){
@@ -11,14 +11,23 @@ int main(){
             cin >> xi[i][j];
             if (N_max[i] < xi[i][j])
             N_max[i] = xi[i][j];
-            }
-        S += N_max[i];
         }
+        S += N_max[i];
+    }
     cout << S << endl;
     
     for (int i = 0; i < N; ++i)
-        if (S % N_max[i] == 0)
-            cout << N_max[i];
+        if (S % N_max[i] == 0){
+            if (first == 1){
+                cout << N_max[i];
+                divisible  = 1;
+                first = 0;
+            }
+            else 
+                cout << " " << N_max[i];
+        }
+    if (divisible == 0)
+        cout << -1;
 
     return 0;
 }
