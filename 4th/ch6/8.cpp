dec593ffa883270833663a49b1ddf16fd0fa04df
Maverick <mike940613@gmail.com>
2025-04-13 08:43:00 +0800

    update

--- 4th/ch6/8.cpp
+++ 4th/ch6/8.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 using namespace std;
 
+class Money{
+    public:
+        int getDollars();
+        int getCents();
+        void setDollars(double iTemp);
+        void setCents(double iTemp);
+        double monetaryAmount();
+    private:
+        int dollars;
+        int cents;
+};
+
 int main(){
-    
+    char repeat;
+    Money money1, money2;
+    double iTemp;
+
+    do{
+        cout << "Monetary amount => ";
+        cin >> iTemp;
+        money1.setDollars(iTemp);
+        money1.setCents(iTemp);
+        cout << money1.monetaryAmount();
+        cout << endl;
+
+        cout << "Monetary amount => ";
+        cin >> iTemp;
+        money2.setDollars(iTemp);
+        money2.setCents(iTemp);
+        cout << money2.monetaryAmount();
+        cout << endl;
+
+        cout << endl << "Repeat?(y/n) => ";
+        cin >> repeat;
+    }while (repeat == 'y');
     
     return 0;
+}
+
+int Money::getDollars(){
+    return dollars;
+}
+
+int Money::getCents(){
+    return cents;
+}
+
+void Money::setDollars(double iTemp){
+    dollars = (int) iTemp;
+}
+
+void Money::setCents(double iTemp){
+    cents = (iTemp - (int) iTemp) * 100;
+}
+
+double Money::monetaryAmount(){
+    return dollars + cents * 0.01;
 }
 
 /*
