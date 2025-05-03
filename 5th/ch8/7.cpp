3ddfad86b97cd5ccf0873804b27be07daf4ec7c7
Maverick <mike940613@gmail.com>
2025-05-04 04:37:40 +0800

    Initial upload

---
+++ 5th/ch8/7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
+
+/*
+    Define a class named primeNumber that stores a prime number. 
+    The default constructor should set the prime number to 1. 
+    Add another constructor that allows the caller to set the prime number. 
+    Also, add a function to het the prime number. 
+    Finally, overload the prefix and postfix ++ and -- operators 
+    so they return a PrimeNumber object that is the next largest (for ++) and the next smallest prime number (for --). 
+    For example, if the object's prime number is set to 13, then invoking ++ should return a primeNumber object whose prime number is set to 17. 
+    Create an appropriate test program for the class. 
+
+*/
---
+++ 5th/ch9/4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
+
+/*
+Write a program that reads in a line of text and replaces all four-letter words with the word "love". 
+For example, the input string
+    I hate you, you dodo!
+should produce the following output:
+    I love you, you love!
+Of course, the output will not always make sense. 
+For example, the input string
+    John will run home. 
+should produce the following output: 
+    Love love run love. 
+If the four-letter word starts with a capital letter, it should be replaced by "Love", not by "love". 
+You need not check capitalization, except for the first letter of a word. 
+A word is any string consisting of the letter of the alphabet and delimited at each end by a blank, 
+the end of the line, or any other character that is not a letter.
+Your program should repeat this action until the user says to quit. 
+*/
