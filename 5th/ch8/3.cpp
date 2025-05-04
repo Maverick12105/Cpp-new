ffdd1fcfee38b92a65dd4ae28d65f59511c863f4
Maverick <mike940613@gmail.com>
2025-05-04 09:10:09 +0800

    Update

--- 5th/ch8/3.cpp
+++ 5th/ch8/3.cpp
@@ -1,17 +1,72 @@
 #include <iostream>
 using namespace std;
 
+class Complex{
+    public:
+        Complex(): real(0), imaginary(0) {} 
+        Complex(double realPart): real(realPart), imaginary(0) {}
+        Complex(double realPart, double imaginaryPart): real(realPart), imaginary(imaginaryPart) {}
+        friend bool operator == (const Complex& param1, const Complex& param2);
+        friend Complex operator + (const Complex& param1, const Complex& param2);
+        friend Complex operator - (const Complex& param1, const Complex& param2);
+        friend Complex operator * (const Complex& param1, const Complex& param2);
+        friend istream& operator >> (istream& paramIn, Complex& param);
+        friend ostream& operator << (ostream& paramOut, const Complex& param);
+    private:
+        double real;
+        double imaginary;
+};
+
 int main(){
-    cout << "hello world";
+    const Complex i(0, 1);
+    Complex a(1), b(5, 13), c;
+
+    cout << b << endl;
+    cout << a * i << endl << b * i << endl;
+    cout << b + a << endl << b - a << endl;
+    cout << (a == b) << endl;
+
+    cin >> c;
+    cout << c << endl;
 
     return 0;
+}
+
+bool operator == (const Complex& param1, const Complex& param2){
+    if (param1.real == param2.real && param1.imaginary == param2.imaginary)
+        return true;
+    else
+        return false;
+}
+
+Complex operator + (const Complex& param1, const Complex& param2){
+    return Complex(param1.real + param2.real, param1.imaginary + param2.imaginary);
+}
+
+Complex operator - (const Complex& param1, const Complex& param2){
+    return Complex(param1.real - param2.real, param1.imaginary - param2.imaginary);
+}
+
+Complex operator * (const Complex& param1, const Complex& param2){
+    return Complex(param1.real * param2.real - param1.imaginary * param2.imaginary,
+                   param1.real * param2.imaginary + param1.imaginary * param2.real);
+}
+
+istream& operator >> (istream& paramIn, Complex& param){
+    cin >> param.real >> param.imaginary;
+    return paramIn;
+}
+
+ostream& operator << (ostream& paramOut, const Complex& param){
+    cout << param.real << " × " << param.imaginary << 'i';
+    return paramOut;
 }
 
 /*
     Define a class for complex numbers. 
     A complex number is a number of the form
         a + b * i
-    where for out purposes, a and b are numbers of type double, and i is a number that represents the quantity √-1. 
+    where for our purposes, a and b are numbers of type double, and i is a number that represents the quantity √-1. 
     Repersent a complex number as two values of type double. 
     Name the member variable real and imaginary. (The variable for the number that is multiplied by i is the one called imaginary.)
     Call the class Complex. 
